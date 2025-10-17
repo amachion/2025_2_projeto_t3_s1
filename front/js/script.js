22245c5607dabe86e38050265346bbbf79e5d746
Andreia Machion <amachion@gmail.com>
2025-10-17 12:51:24 -0300

    apostila 09 até código 2.13.2

--- front/js/script.js
+++ front/js/script.js
@@ -18,3 +18,27 @@ async function obtemFilmes() {
         celulaSinopse.innerHTML = filme.sinopse
     }
 }
+async function cadastrarFilme() {
+    //montamos a URL completa
+    const URLcompleta = `${protocolo}${baseURL}${filmesEndpoint}`
+    //pega os inputs digitados pelo usuário
+    let tituloInput = document.querySelector('#tituloInput')
+    let sinopseInput = document.querySelector('#sinopseInput')
+    let titulo = tituloInput.value
+    let sinopse = sinopseInput.value
+    //limpa as caixinhas de input
+    tituloInput.value = ""
+    sinopseInput.value = ""
+    const filmes = (await axios.post(URLcompleta, {titulo, sinopse})).data
+    //limpar a tabela e preencher com a lista nova de filmes
+    let tabela = document.querySelector('.filmes')
+    let corpoTabela = tabela.getElementsByTagName('tbody')[0]
+    corpoTabela.innerHTML = ""
+    for (let filme of filmes) {
+        let linha = corpoTabela.insertRow(0)
+        let celulaTitulo = linha.insertCell(0)
+        let celulaSinopse = linha.insertCell(1)
+        celulaTitulo.innerHTML = filme.titulo
+        celulaSinopse.innerHTML = filme.sinopse
+    }
+}
